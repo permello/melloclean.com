a6325a2dfca5b3b559b4c632c8349d292f092fb9
Eduardo Turcios <67609856+Ender-swe@users.noreply.github.com>
2026-02-07 14:23:33 -0600

    added debuging

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  server: {
+    port: 3000,
+    strictPort: true,
+    open: false,
+  },
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
